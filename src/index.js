43f5f64c6097493d228386f1c190062d4e1df44a
SaileshBK <101400043+SaileshBK@users.noreply.github.com>
2022-04-08 22:07:05 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+import App from './App'
+
+//mounting the app to the root div.
+//Note : necessary for every single react app.
+ReactDOM.render(<App />, document.getElementById('root'));
